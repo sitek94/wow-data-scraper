6ed63e954e8b0367e9a79f64fd2ed81aedb9ddea
Maciek <msitkowski94@gmail.com>
2022-02-20 09:23:18 +0100

    init puppeteer

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log('Hello World')
+import puppeteer from 'puppeteer'
+
+const baseUrl = 'https://www.wowhead.com'
+
+const browser = await puppeteer.launch()
+const page = await browser.newPage()
+await page.goto(baseUrl + '/achievements')
+await page.screenshot({ path: 'example.png' })
+
+await browser.close()
