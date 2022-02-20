b9efab5166701c1c38e2a39ed6cbdb8e116ff459
Maciek <msitkowski94@gmail.com>
2022-02-20 13:14:06 +0100

    extract achievements scraper to separate module

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import puppeteer from 'puppeteer'
 import { acceptCookies, declineNotifications } from './utils'
+import { scrapeAchievementsPage } from 'src/achievements'
 
 const baseUrl = 'https://www.wowdb.com'
 
@@ -15,58 +16,9 @@ async function run() {
   await acceptCookies(page)
   await declineNotifications(page)
 
-  const rowHandles = await page.$x('//*[@id="achievements"]/tbody/tr')
+  const achievements = await scrapeAchievementsPage(page)
 
-  /*
-    Table structure, has the following structure, so it's "very convenient" to
-    search for the data we want 🙈
-
-    <tr>
-      <td class="col-name">   - Name
-      <td>                    - Description
-      <td class="col-c">      - Fraction
-      <td>                    - Points
-      <td class="col-c>       - Category
-    </tr>
-  */
-  for (const rowHandle of rowHandles) {
-    const iconUrl = await rowHandle.$eval(
-      '.listing-icon img',
-      (el: HTMLImageElement) => el.src,
-    )
-    const name = await rowHandle.$eval('a.t', el => el.textContent)
-    const description = await rowHandle.$eval('.subtext', el => el.textContent)
-
-    const fractionHtml = await rowHandle.$eval(
-      'td.col-name + td',
-      el => el.innerHTML,
-    )
-    let fraction = null
-    if (fractionHtml) {
-      fraction = /horde/.test(fractionHtml) ? 'horde' : 'alliance'
-    }
-
-    let points = null
-    const pointsHtml = await rowHandle.$eval('td.col-c', el => el.innerHTML)
-    if (pointsHtml) {
-      points = Number(pointsHtml.replace(/[^0-9]/g, ''))
-    }
-
-    const category = await rowHandle.$eval(
-      'td.col-c:last-child',
-      el => el.textContent,
-    )
-
-    const achievement = {
-      name,
-      description,
-      iconUrl,
-      fraction,
-      points,
-      category,
-    }
-    console.log(achievement)
-  }
+  console.log(achievements)
 
   await browser.close()
 }
