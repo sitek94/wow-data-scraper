bd527ea1eb08cf136060597203d2daa811169fc0
Maciek <msitkowski94@gmail.com>
2022-02-20 13:08:39 +0100

    scrape full Achievement object

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import path from 'path'
 import puppeteer from 'puppeteer'
 import { acceptCookies, declineNotifications } from './utils'
 
-const baseUrl = 'https://www.wowhead.com'
+const baseUrl = 'https://www.wowdb.com'
 
 run()
 
@@ -11,21 +10,63 @@ async function run() {
     process.env.DEBUG ? { headless: false, slowMo: 250 } : {},
   )
   const page = await browser.newPage()
-
-  await page.goto(url('/character-achievements/quests/eastern-kingdoms'))
+  await page.goto(`${baseUrl}/achievements`)
 
   await acceptCookies(page)
   await declineNotifications(page)
 
-  const [el] = await page.$x(
-    '//*[@id="lv-achievements"]/div[2]/div/table/tbody/tr[2]/td[2]/a',
-  )
-  const title = await el.getProperty('textContent')
-  console.log(await title.jsonValue())
+  const rowHandles = await page.$x('//*[@id="achievements"]/tbody/tr')
+
+  /*
+    Table structure, has the following structure, so it's "very convenient" to
+    search for the data we want 🙈
+
+    <tr>
+      <td class="col-name">   - Name
+      <td>                    - Description
+      <td class="col-c">      - Fraction
+      <td>                    - Points
+      <td class="col-c>       - Category
+    </tr>
+  */
+  for (const rowHandle of rowHandles) {
+    const iconUrl = await rowHandle.$eval(
+      '.listing-icon img',
+      (el: HTMLImageElement) => el.src,
+    )
+    const name = await rowHandle.$eval('a.t', el => el.textContent)
+    const description = await rowHandle.$eval('.subtext', el => el.textContent)
+
+    const fractionHtml = await rowHandle.$eval(
+      'td.col-name + td',
+      el => el.innerHTML,
+    )
+    let fraction = null
+    if (fractionHtml) {
+      fraction = /horde/.test(fractionHtml) ? 'horde' : 'alliance'
+    }
+
+    let points = null
+    const pointsHtml = await rowHandle.$eval('td.col-c', el => el.innerHTML)
+    if (pointsHtml) {
+      points = Number(pointsHtml.replace(/[^0-9]/g, ''))
+    }
+
+    const category = await rowHandle.$eval(
+      'td.col-c:last-child',
+      el => el.textContent,
+    )
+
+    const achievement = {
+      name,
+      description,
+      iconUrl,
+      fraction,
+      points,
+      category,
+    }
+    console.log(achievement)
+  }
 
   await browser.close()
-}
-
-function url(...paths: string[]) {
-  return path.join(baseUrl, ...paths)
 }
